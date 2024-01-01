75316ce166b17c2125139d2948dd2af662c7b064
KingZoldack <jeanpaulismael@gmail.com>
2024-01-01 20:42:35 +0000

    Initial Commit
    Initial Commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ColorGenerator
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ColorGenerator
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
